bbb745f1198dcf2d911512e8b33d0c63546ea378
Akzhol Asanov <asanov.akzhol04@gmail.com>
2023-06-30 11:24:15 +0600

    username

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
-export const API = "http://13.48.195.126/";
-export const API_AUTH = "http://13.48.195.126/auth/";
+export const API = "http://16.171.23.239/";
+export const API_AUTH = "http://16.171.23.239/auth/";
